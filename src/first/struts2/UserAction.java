8d7122e81c71012dfb9801abf018c22b4872918e
qeeguan <half_a_dream@qq.com>
2017-05-29 17:44:16 +0800

    动态Action
    Signed-off-by: qeeguan <half_a_dream@qq.com>

---
+++ src/first/struts2/UserAction.java
@@ -0,0 +1,31 @@
+package first.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1715770603901198863L;
+
+	private String info;
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String add() {
+		info = "添加用户信息";
+		return "add";
+	}
+
+	public String update() {
+		info = "更新用户信息";
+		return "update";
+	}
+}
